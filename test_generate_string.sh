4e1a37bb0d2559058e4dacb31107d8de40d0505e
ion <ion@hashicorp.com>
2021-09-13 10:03:15 +0200

    Update test_generate_string.sh

--- test_generate_string.sh
+++ test_generate_string.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 # This is made by Ion
 
+cat generate_string.sh
+
 RES1 = `bash generate_string.sh`
 
-if ["$RES1" == "ABCDEFG"] ;then
+if [ $RES1 == "ABCDEFG" ] ;then
   echo "PASSED THE TEST"
   exit 0
 else
